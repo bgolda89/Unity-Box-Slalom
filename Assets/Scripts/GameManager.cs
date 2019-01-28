573c559262110c2f1eefabf8c112a95306fb1a4c
bgolda89 <42428563+bgolda89@users.noreply.github.com>
2019-01-28 15:37:57 -0700

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+    float restartDelay = 2f;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel ()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame () {
+        if (gameHasEnded == false) 
+        {
+            Debug.Log("gameOver");
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+
+    } 
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
